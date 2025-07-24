f4ae3df9234758800b103e460d2bfd6ee2573347
lrmn7 <lrmn.dev@gmail.com>
2025-07-24 16:22:55 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+  <h1 align="center">Build the Quiz, Break the Minds</h1>
+  <h3 align="center">Your Quiz, Your Rules. The World Plays.</h3>
+</p>
+
+<p align="center">
+  <img src="https://fun-quiz.fun/og-fun-quiz.png" alt="Quiz OG Banner" width="1280"/>
+</p>
--- contracts/FunQuiz.sol
+++ contracts/FunQuiz.sol
@@ -5,17 +5,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title FunQuiz
- * @author Partner Coding
- * @notice Versi 2: Menambahkan biaya untuk bermain kuis.
+ * @author L RMN
  */
 contract FunQuiz is Ownable {
-    // --- State Variables ---
-
     uint256 public createQuizFee;
-    uint256 public playQuizFee; // BIAYA BARU UNTUK BERMAIN
+    uint256 public playQuizFee;
     uint256 public quizCounter;
-
-    // --- Structs ---
 
     struct Question {
         string questionText;
@@ -32,105 +27,89 @@ contract FunQuiz is Ownable {
         Question[10] questions;
     }
 
-    struct PlayerScore {
-        address player;
-        uint256 score;
-    }
-
-    // --- Mappings ---
-
     mapping(uint256 => Quiz) public quizzes;
-    mapping(uint256 => PlayerScore[]) public leaderboards;
-    mapping(uint256 => mapping(address => bool)) public hasPlayed;
-    
-    // MAPPING BARU: Untuk melacak siapa yang sudah bayar untuk bermain kuis tertentu
     mapping(uint256 => mapping(address => bool)) public hasPaidToPlay;
-
-    // --- Events ---
+    mapping(uint256 => mapping(address => uint256)) public playerScores;
+    mapping(uint256 => address[]) public quizParticipants;
+    mapping(uint256 => mapping(address => bool)) private isParticipant;
 
     event QuizCreated(uint256 indexed quizId, address indexed creator, string title);
-    event ScoreSubmitted(uint256 indexed quizId, address indexed player, uint256 score);
+    event ScoreUpdated(uint256 indexed quizId, address indexed player, uint256 newScore);
     event FeeUpdated(string feeType, uint256 newFee);
     event Withdrawn(address indexed to, uint256 amount);
-    event PlayFeePaid(uint256 indexed quizId, address indexed player); // EVENT BARU
-
-    // --- Constructor ---
+    event PlayFeePaid(uint256 indexed quizId, address indexed player);
 
     constructor(uint256 _initialCreateFee, uint256 _initialPlayFee) Ownable(msg.sender) {
         createQuizFee = _initialCreateFee;
         playQuizFee = _initialPlayFee;
-        emit FeeUpdated("create", _initialCreateFee);
-        emit FeeUpdated("play", _initialPlayFee);
     }
 
-    // --- Functions ---
-    
-    /**
-     * @notice FUNGSI BARU: Pengguna membayar untuk mendapatkan akses bermain kuis.
-     * @param _quizId ID kuis yang ingin dimainkan.
-     */
     function payToPlay(uint256 _quizId) external payable {
         require(quizzes[_quizId].creator != address(0), "FunQuiz: Kuis tidak ditemukan.");
         require(msg.value == playQuizFee, "FunQuiz: Biaya bermain tidak sesuai.");
-        require(!hasPaidToPlay[_quizId][msg.sender], "FunQuiz: Anda sudah membayar untuk kuis ini.");
-
-        hasPaidToPlay[_quizId][msg.sender] = true;
-        emit PlayFeePaid(_quizId, msg.sender);
+        if (!hasPaidToPlay[_quizId][msg.sender]) {
+            hasPaidToPlay[_quizId][msg.sender] = true;
+            emit PlayFeePaid(_quizId, msg.sender);
+        }
     }
 
-    function createQuiz(
-        string memory _title,
-        Question[10] memory _questions
-    ) external payable {
+    function createQuiz(string memory _title, Question[10] memory _questions) external payable {
         require(msg.value == createQuizFee, "FunQuiz: Biaya pembuatan kuis tidak sesuai.");
         quizCounter++;
         uint256 newQuizId = quizCounter;
-        quizzes[newQuizId] = Quiz({
-            id: newQuizId,
-            title: _title,
-            creator: msg.sender,
-            questions: _questions
-        });
+
+        Quiz storage newQuiz = quizzes[newQuizId];
+        newQuiz.id = newQuizId;
+        newQuiz.title = _title;
+        newQuiz.creator = msg.sender;
+
+        for (uint i = 0; i < _questions.length; i++) {
+            newQuiz.questions[i] = _questions[i];
+        }
+
         emit QuizCreated(newQuizId, msg.sender, _title);
     }
 
-    /**
-     * @notice DIMODIFIKASI: Sekarang memeriksa apakah pengguna sudah bayar.
-     */
     function submitScore(uint256 _quizId, uint256 _score) external {
         require(quizzes[_quizId].creator != address(0), "FunQuiz: Kuis tidak ditemukan.");
-        require(hasPaidToPlay[_quizId][msg.sender], "FunQuiz: Anda harus membayar untuk bermain kuis ini."); // PENGECEKAN BARU
-        require(!hasPlayed[_quizId][msg.sender], "FunQuiz: Anda sudah submit skor untuk kuis ini.");
-
-        hasPlayed[_quizId][msg.sender] = true;
-        leaderboards[_quizId].push(PlayerScore({
-            player: msg.sender,
-            score: _score
-        }));
-        emit ScoreSubmitted(_quizId, msg.sender, _score);
+        require(hasPaidToPlay[_quizId][msg.sender], "FunQuiz: Anda harus membayar untuk bermain kuis ini.");
+        
+        uint256 existingScore = playerScores[_quizId][msg.sender];
+        
+        if (_score > existingScore) {
+            playerScores[_quizId][msg.sender] = _score;
+            if (!isParticipant[_quizId][msg.sender]) {
+                quizParticipants[_quizId].push(msg.sender);
+                isParticipant[_quizId][msg.sender] = true;
+            }
+            emit ScoreUpdated(_quizId, msg.sender, _score);
+        }
     }
-
-    // --- View Functions ---
-    // (Tidak ada perubahan pada view functions: getQuizById, getLeaderboard, getContractBalance)
 
     function getQuizById(uint256 _quizId) external view returns (Quiz memory) {
         return quizzes[_quizId];
     }
 
-    function getLeaderboard(uint256 _quizId) external view returns (PlayerScore[] memory) {
-        return leaderboards[_quizId];
+    function getQuestionsByQuizId(uint256 _quizId) external view returns (Question[10] memory) {
+        require(quizzes[_quizId].creator != address(0), "FunQuiz: Kuis tidak ditemukan.");
+        return quizzes[_quizId].questions;
+    }
+
+    function getLeaderboardData(uint256 _quizId) external view returns (address[] memory addresses, uint256[] memory scores) {
+        address[] memory participants = quizParticipants[_quizId];
+        scores = new uint256[](participants.length);
+
+        for (uint i = 0; i < participants.length; i++) {
+            scores[i] = playerScores[_quizId][participants[i]];
+        }
+
+        return (participants, scores);
     }
 
     function getContractBalance() external view returns (uint256) {
         return address(this).balance;
     }
 
-    // --- Admin Functions ---
-
-    /**
-     * @notice FUNGSI BARU: Mengatur biaya untuk bermain kuis.
-     * @param _newFee Biaya baru dalam wei.
-     */
     function setPlayQuizFee(uint256 _newFee) external onlyOwner {
         playQuizFee = _newFee;
         emit FeeUpdated("play", _newFee);
